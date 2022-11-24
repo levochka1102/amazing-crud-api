29677d0560a05a0bbe1b30f1dee49a9a32cf1133
lev <l.mochalov@yarus.pro>
2022-11-24 12:38:54 +0500

    migrations, models was created

---
+++ database/migrations/2022_11_24_115916_create_game_genre_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Game;
+use App\Models\Genre;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('game_genre', function (Blueprint $table) {
+            $table->foreignIdFor(Game::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Genre::class)->constrained()->onDelete('cascade');
+            $table->primary(['game_id', 'genre_id']);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('game_genre');
+    }
+};
