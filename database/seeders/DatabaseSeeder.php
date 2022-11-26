95bd4d35f5705a8f3a061033dcd60c65f55b961e
lev <l.mochalov@yarus.pro>
2022-11-27 01:07:16 +0500

    reworking seeder for pivot table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Developer;
+use App\Models\Game;
+use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +17,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            GenreSeeder::class,
-            DeveloperSeeder::class,
-            GameSeeder::class,
-        ]);
+//        $this->call([
+//            GenreSeeder::class,
+//            DeveloperSeeder::class,
+//            GameSeeder::class,
+//        ]);
+
+        $genresNames = [
+            'Ужасы', 'Детектив', 'Шутер', 'Рогалик', 'Приключение', 'Платформер', 'Головоломки', 'Аркады'
+        ];
+
+        $genres = Genre::factory()
+            ->count(count($genresNames))
+            ->sequence(fn ($sequence) => ['name' => $genresNames[$sequence->index]])
+            ->create();
+
+        Developer::factory(80)->create()->each(function ($developer) use ($genres) {
+            Game::factory(rand(1, 4))->create([
+                'developer_id' => $developer->id
+            ])->each(function ($game) use ($genres) {
+                $game->genres()->attach($genres->random(rand(1, 3)));
+            });
+        });
+
+
+
     }
 }
